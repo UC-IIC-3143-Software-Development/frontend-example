61cae23b57d002096d0904277c4b790d2740e07e
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2022-03-29 22:22:21 -0300

    chore(msw): eslint ignore export rules per file

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/no-extraneous-dependencies */
 import { setupServer } from "msw/node";
 
 import { handlers } from "./handlers";
