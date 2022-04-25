eb133c0b791634af497677119e749af6c5473ef5
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2022-04-25 05:59:13 -0400

    chore: test userLists with msw

--- src/components/usersListsFromAPI.test.js
+++ src/components/usersListsFromAPI.test.js
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import UsersListsFromAPI from "./usersListsFromAPI";
 
-test("Given render UserListsFromAPI Then expect heading in the document", () => {
+test("Given render UserListsFromAPI Then expect a mock user in the document", async () => {
   // Given
   render(<UsersListsFromAPI />);
   const userHeader = screen.getByRole("heading", { name: /users/i });
   expect(userHeader).toBeInTheDocument();
+
+  const text = await screen.findByText("Gonzalo");
+  expect(text).toBeInTheDocument();
 });
